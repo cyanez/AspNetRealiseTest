acd2918605d168e872d803f10a19ae5b72314f78
cyanez <chris.yanez@gmail.com>
2020-10-23 02:45:55 -0500

    Agregar archivos de proyecto.

---
+++ Test/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+using WebNet.Models;
+
+namespace Test {
+  public class UnitTest {
+    [Fact]
+    public void TestSayHello() {
+     Employe employee = new Employe();
+
+      employee.Name = "Christian";
+      var returnValue = employee.SayHello();
+
+      Assert.Equal("hello Christian", returnValue);
+    }
+  }
+}
---
+++ WebNet/Models/Employe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace WebNet.Models {
+  public class Employe {
+    private string name;
+    public string Name {
+      get => name;
+      set => name = value;
+    }
+
+    public string SayHello() {
+      return $"hello {name}";
+    }
+  }
+}
